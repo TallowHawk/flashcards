5ec4dfe8dab5fcfa0b6e908bc1d62fd9fb039e71
Todd Bednarczyk <echo@dronda.org>
2021-01-09 18:01:56 -0500

    Revert public path

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
         globalObject: "(typeof self!='undefined'?self:global)",
-        publicPath: '/flashcards',
+        publicPath: '/',
     },
 
     devtool: 'eval-source-map',
